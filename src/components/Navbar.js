e729702e5b7422cd188af14ef442af09745b5ed2
Rif-7 <muhammadrifan974530@gmail.com>
2022-05-01 21:40:14 +0530

    add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <span className="header">Stock Viewer</span>
+      <span className="view-all">View All Products</span>
+    </nav>
+  );
+};
+
+export default Navbar;
